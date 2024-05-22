9b4791095ea61969d226db0f5091d3b49f7f8907
bipul-roy <bipulcartmade@gmail.com>
2024-05-22 15:27:45 +0545

    default image new

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ function appendDiv(sku, variant_data) {
     // Function to create product cards
     function createProductCards(products) {
         return products.map(product => {
-            const imageUrl = product.defaultImage ? product.defaultImage.url : 'http://localhost:7755/static/asset/ProductDefault.gif';
+            const imageUrl = product.defaultImage ? product.defaultImage.url : 'http://localhost:7755/static/asset/CategoryDefault.png';
             return `
         <div class="related-products-swiper-slide swiper-slide">
           <div class="product-card">
