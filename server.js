d9dc818335bd9faa11d2a0b1c85808b597c31f11
bipul-roy <bipulcartmade@gmail.com>
2024-06-17 16:03:39 +0545

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const multer = require('multer');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
@@ -12,7 +13,51 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 app.use('/api', storeRoutes);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Search auto backend API...');
+});
+
+
+// File upload
+// Configure multer for file upload
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Ensure this folder exists
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 5 * 1024 * 1024 }, // 1 MB file size limit
+    fileFilter: function (req, file, cb) {
+        const fileTypes = /xlsx|application\/vnd\.openxmlformats-officedocument\.spreadsheetml\.sheet/;
+        const mimeType = fileTypes.test(file.mimetype);
+        const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+
+        if (mimeType && extName) {
+            return cb(null, true);
+        }
+        cb(new Error('File type not supported'));
+    }
+});
+
+// Endpoint to handle file upload
+app.post('/files/upload', upload.single('file'), (req, res) => {
+    try {
+        const filePath = `/uploads/${req.file.filename}`;
+        const fileUrl = `${req.protocol}://${req.get('host')}${filePath}`;
+        res.status(200).send({
+            message: 'File uploaded successfully',
+            fileUrl: fileUrl
+        });
+    } catch (err) {
+        res.status(400).send({
+            message: 'Failed to upload file',
+            error: err.message
+        });
+    }
 });
 
 app.listen(port, () => {
